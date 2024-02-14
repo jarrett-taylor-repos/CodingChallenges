92aaf4cd8d2476122243ccd46af0430aea670a36
jarrett-taylor-repos <jarrett.christopher.taylor@gmail.com>
2024-02-14 13:27:50 -0600

    optimize

--- LeetCode/0006 - Zigzag Conversion/main.cpp
+++ LeetCode/0006 - Zigzag Conversion/main.cpp
@@ -3,11 +3,10 @@
 using std::string, std::cout, std::endl;
 
 string convert(string s, int numRows) {
-    if(!numRows || !s.length()) return s;
+    if(!numRows || !s.length() || numRows == 1) return s;
 
     int len = s.length();
     string zigzag = "";
-    zigzag.push_back(s[0]);
     for(int i = 0; i < numRows; i++) {
         int index = 0;
         int step;
@@ -15,25 +14,23 @@ string convert(string s, int numRows) {
         while(index < len){
             step = (numRows*2)-2;
             if(!i) {
-                index += step;
                 zigzag.push_back(s[index]);
             } else if((i+1) % numRows == 0) {
-                zigzag.push_back(s[index+i]);
-                index += step;
+                if(index+i < len) zigzag.push_back(s[index+i]);
             } else {
                 int zagIndex = index + step - i;
                 if(index+i < len) zigzag.push_back(s[index+i]);
                 if(zagIndex < len) zigzag.push_back(s[zagIndex]);
-                index += step;
             }
+            index += step;
         }
-       //zigzag.push_back('\n');
     }
 
     return zigzag;
 }
 
 int main() {
+    cout << convert("AB", 2) << endl;
     cout << convert("PAYPALISHIRING", 3) << endl;
     cout << convert("PAYPALISHIRING", 4) << endl;
     return 0;
