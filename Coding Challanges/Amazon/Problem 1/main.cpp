0c596cf952c3f912512634a22dcd66357e3b652e
jarrett-taylor-repos <jarrett.christopher.taylor@gmail.com>
2024-07-23 09:25:36 -0500

    after test

--- Coding Challanges/Amazon/Problem 1/main.cpp
+++ Coding Challanges/Amazon/Problem 1/main.cpp
@@ -16,14 +16,6 @@ string rtrim(const string &);
  *  2. INTEGER k
  *  3. INTEGER threshold
  */
- 
-int sumVect(vector<int> v) {
-    int sum = 0;
-    for(int i = 0 ; i < v.size(); i++) {
-        sum += v[i];
-    }
-    return sum;
-}
 
 //dynamic programming problem? seems like memoization would be the value of k and threshold 
 //potential backtracking with compliment of threshold - current value
@@ -33,63 +25,7 @@ int sumVect(vector<int> v) {
 
 int reduceGifts(vector<int> prices, int k, int threshold) {
 }
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
 
-    string prices_count_temp;
-    getline(cin, prices_count_temp);
-
-    int prices_count = stoi(ltrim(rtrim(prices_count_temp)));
-
-    vector<int> prices(prices_count);
-
-    for (int i = 0; i < prices_count; i++) {
-        string prices_item_temp;
-        getline(cin, prices_item_temp);
-
-        int prices_item = stoi(ltrim(rtrim(prices_item_temp)));
-
-        prices[i] = prices_item;
-    }
-
-    string k_temp;
-    getline(cin, k_temp);
-
-    int k = stoi(ltrim(rtrim(k_temp)));
-
-    string threshold_temp;
-    getline(cin, threshold_temp);
-
-    int threshold = stoi(ltrim(rtrim(threshold_temp)));
-
-    int result = reduceGifts(prices, k, threshold);
-
-    fout << result << "\n";
-
-    fout.close();
-
+int main() {
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
